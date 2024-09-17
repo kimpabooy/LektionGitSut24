684eeb2ff8bcd96b6041a5a6c44261dd50fed312
kimpabooy <kimpabooy@live.com>
2024-09-17 10:11:48 +0200

    Add project files.

---
+++ LektionGitSut24/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LektionGitSut24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
